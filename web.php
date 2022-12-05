e36ffa8490e0a9428bd8f1d53618b71144692829
pvtam2a <tam.pham@ntq-solution.com.vn>
2022-12-05 18:32:10 +0900

    add web.php

---
+++ web.php
@@ -0,0 +1,3 @@
+Route::get('/v2/cmsgtemplate', 'CmsgtemplateController@index');
+Route::post('/v2/cmsgtemplate', 'CmsgtemplateController@index');
+Route::get('/v2/cevent/about', 'CeventController@about');
