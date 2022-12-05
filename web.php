5be0cb8bf7c3d3f66f46a9c221fb1aa28bef4787
pvtam2a <tam.pham@ntq-solution.com.vn>
2022-12-05 18:34:33 +0900

    fix name route

--- web.php
+++ web.php
@@ -1,3 +1,3 @@
-Route::get('/v2/cmsgtemplate', 'CmsgtemplateController@index');
-Route::post('/v2/cmsgtemplate', 'CmsgtemplateController@index');
-Route::get('/v2/cevent/about', 'CeventController@about');
+Route::get('/v2/cmsgtemplate', 'CmsgtemplateController@index')->name('get_cmsgtemplate');
+Route::post('/v2/cmsgtemplate', 'CmsgtemplateController@index')->name('post_cmsgtemplate');
+Route::get('/v2/cevent/about', 'CeventController@about')->name('get_cevent_about');
